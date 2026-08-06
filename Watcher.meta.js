ba688e63fb03daf099241894b6b2823c810400ae
Rudeboy <67002163+1gby@users.noreply.github.com>
2026-08-05 23:21:07 -0600

    Add files via upload
    Forgot this file lol

---
+++ Watcher.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Watcher by Rudeboy™
+// @namespace    https://rud3boy.vercel.app
+// @version      3.1.0
+// @description  Adds Watcher buttons to IMDB, Letterboxd, Trakt, JustWatch, MDBList, iCheckMovies, TheTVDB, Criticker, Metacritic + copy magnet & open Watcher
+// @author       Rudeboy™
+// @license      MIT
+// @match        *://*/*
+// @grant        none
+// @run-at       document-idle
+// @downloadURL https://rud3boy.vercel.app/Watcher.user.js
+// @updateURL   https://rud3boy.vercel.app/Watcher.meta.js
+// ==/UserScript==
